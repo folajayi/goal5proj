b4f837dfdcd16f34ab1492e950229b3d61df00e0
Fola001 <folaselflearn@gmail.com>
2024-12-02 11:20:44 +0000

    init commit

---
+++ src/test/java/stepDefinitions/OnlineProductsPage.java
@@ -0,0 +1,21 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+import static pages.OnlineProductsPage.click_formalshoesdropdpown;
+import static pages.OnlineProductsPage.getText_formalshoes_firstvalue;
+
+public class OnlineProductsPage {
+    @When("User clicks on Formal Shoes drop down")
+    public void user_clicks_on_Formal_Shoes_drop_down() throws InterruptedException {
+        click_formalshoesdropdpown();
+    }
+    @Then("User should be able to view the Products")
+    public void user_should_be_able_to_view_the_Products() throws InterruptedException {
+        getText_formalshoes_firstvalue();
+        assertEquals("   Classic Cheltenham",getText_formalshoes_firstvalue());
+    }
+
+}
